b8080c055fc8facd921c20ee0b21a55af8ca6e92
Mika Fayeulle <mika.fayeulle@gmail.com>
2020-09-03 13:17:34 +0200

    API call + logic v2

--- src/components/Meteo.vue
+++ src/components/Meteo.vue
@@ -6,20 +6,20 @@
             <label for="position">Entrer le nom de la ville</label>
             <input 
                 type="text" 
-                class="form-control"
+                class="form-control w-75 m-auto"
                 id="position"
-                v-model="request"
-                v-on:keypress="goMeteo"
+                v-model="requete"
+                v-on:keypress.enter="goMeteo"
             >
         </div>
-        <div class="w-75 m-auto">
-            <h3 class="text-center mb-3">Position: Paris</h3>
-            <div class="card text-center p-5">
+        <div v-if="temps" class="w-75 m-auto">
+            <h3 class="text-center mb-3">Position: {{ temps.name}}</h3>
+            <div  class="card text-center p-5">
                 <p class="texte-affichage">
-                    Temperature: 26 deg
+                    Temperature: {{temps.main.temp.toFixed()}} degrès
                 </p>
                 <p class="texte-affichage">
-                    Temps: Nuageux
+                    Temps: {{temps.weather[0].description}}
                 </p>
             </div>
         </div>
@@ -27,16 +27,36 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'Meteo',
     data(){
         return{
+            requete: '',
+            temps: undefined,
+            api_code: '0f24f9ceae74774fd8c3f6ec9730fbe6',
+            url_appel: 'https://api.openweathermap.org/data/2.5/weather?'
+        }
+    },
 
+    methods: {
+        goMeteo: function(){
+            axios
+            .get(`${this.url_appel}q=${this.requete}&units=metric&APPID=${this.api_code}&lang=fr`)
+            .then(response => {
+                this.temps = response.data
+                console.log(response)
+                this.requete = ""
+            })
         }
     }
 }
 </script>
 
 <style scoped>
-
+.texte-affichage{
+    font-size: 25px;
+    font-weight: 600;
+}
 </style>
